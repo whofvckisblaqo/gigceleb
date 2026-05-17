04d28795fec7f5a6e70e588c3262e994dffdd009
kingsleyonyekwelu <kimonyekwelu@gmail.com>
2026-05-17 09:52:46 +0100

    Add Tawk.to live chat for Gigceleb

--- src/components/ui/TawkTo.js
+++ src/components/ui/TawkTo.js
@@ -10,7 +10,7 @@ export default function TawkTo() {
       var s1 = document.createElement("script");
       var s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/69f1bb649569db1c3432caee/1jnc480nh";
+      s1.src = "https://embed.tawk.to/6a098126c744531c437324c1/1joqi103k";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
